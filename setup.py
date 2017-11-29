1a4fdf2bbf78fb6ce9cbe255bb3a87dcea19f1a9
estrain <estrain@gmail.com>
2017-11-29 18:46:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
-from distutils.core import setup
-setup(name='seqsero',
-      version='1.0',
-      py_modules=['seqsero'],
-      )
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "seqsero",
+    version = "1.0",
+    author = "DengLab",
+    author_email = "estrain@gmail.com",
+    description = ("Serotyping for Salmonella."),
+    license = "GPL-2.0",
+    keywords = "salmonella",
+    url = "https://github.com/estrain/SeqSero",
+    packages=['seqseqo',],
+
+)
